b757502ff5f03de0583bcc398c521b7c7e37edd0
lifanova <veralifanovaa@gmail.com>
2021-05-06 10:39:37 +0300

    Tests testNG are added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemain() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 5800;
+        int actual = cashback.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainWithZeroValue() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainWithEqualValue() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainWithBigValue() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 50000;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
